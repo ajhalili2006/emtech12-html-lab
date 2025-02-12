52e2daa0c967f11145aa395184b3d975048c4a52
Andrei Jiroh Halili <ajhalili2006@andreijiroh.dev>
2025-02-12 08:25:52 +0000

    setup hono-based preview server for codespaces

---
+++ index.ts
@@ -0,0 +1,18 @@
+import { serve } from '@hono/node-server'
+import { Hono } from 'hono'
+import { serveStatic } from '@hono/node-server/serve-static'
+
+const app = new Hono()
+
+// https://hono.dev/docs/getting-started/nodejs#serve-static-files
+app.use(serveStatic({
+  root: "./src"
+}))
+
+const port = Number(process.env.PORT) || 3000
+console.log(`Preview server is running on http://localhost:${port}, you may need to manually reload your browser tab to see frontend changes.`)
+
+serve({
+  fetch: app.fetch,
+  port
+})
